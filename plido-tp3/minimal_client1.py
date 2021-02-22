ab6c2b8a0ab28ddbdd1d092500af1211dabe5936
Laurent Toutain <laurent@touta.in>
2021-02-22 19:05:00 +0100

    correct an error

--- plido-tp3/minimal_client1.py
+++ plido-tp3/minimal_client1.py
@@ -1,12 +1,8 @@
-from virtual_sensor import virtual_sensor 
 import time
 import socket
 
-temperature = virtual_sensor(start=20, variation = 0.1)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    t = temperature.read_value()
-
-    s.sendto (t, ("127.0.0.1", 33033))
+    s.sendto ("message", ("192.168.1.47", 33033))
     time.sleep(10)
