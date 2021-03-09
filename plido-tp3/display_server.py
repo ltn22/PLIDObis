d780fb9db9911a55521e10bb7f6099a2c4288ddc
Laurent Toutain <laurent@touta.in>
2021-03-09 18:38:18 +0100

    files for tp3

--- plido-tp3/display_server.py
+++ plido-tp3/display_server.py
@@ -2,7 +2,7 @@ import socket
 import binascii
 import cbor2 as cbor
 import beebotte
-import config_bbt #secret keys 
+import config_bbt #secret keys
 import datetime
 import time
 import pprint
@@ -14,19 +14,19 @@ bbt = beebotte.BBT(config_bbt.API_KEY, config_bbt.SECRET_KEY)
 
 def to_bbt(channel, res_name, cbor_msg, factor=1, period=10, epoch=None):
     global bbt
-    
+
     prev_value = 0
     data_list = []
     if epoch:
         back_time = epoch
     else:
         back_time = time.mktime(datetime.datetime.now().timetuple())
-    
+
     back_time -= len(cbor_msg)*period
 
     for e in cbor_msg:
         prev_value += e
-        
+
         back_time += period
 
         data_list.append({"resource": res_name,
@@ -34,7 +34,7 @@ def to_bbt(channel, res_name, cbor_msg, factor=1, period=10, epoch=None):
                           "ts": back_time*1000} )
 
     pprint.pprint (data_list)
-    
+
     bbt.writeBulk(channel, data_list)
 
 
@@ -42,4 +42,4 @@ while True:
     data, addr = s.recvfrom(1500)
 
     j = cbor.loads(data)
-    to_bbt("capteurs", "humidity", j, factor=0.01)
+    to_bbt("capteurs", "temperature", j, factor=0.01)
