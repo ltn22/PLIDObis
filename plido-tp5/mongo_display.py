5b6cbb61230ec715d9ef8f8e76d726a2733a336b
Laurent Toutain <ltn22@github.com>
2021-04-25 16:44:22 +0200

    display

---
+++ plido-tp5/mongo_display.py
@@ -0,0 +1,68 @@
+from pymongo import MongoClient
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
+client = MongoClient()
+db = client["meteo-data"]
+measure = db.measure
+
+list_of_locations = measure.find({"@context": "http://user.ackl.io/schema/Sensor"})
+
+sensor_location = "Room A123"
+
+found_item = measure.find_one ({"Location" : sensor_location })
+if found_item == None:
+    raise ValueError ("{} not found".format(sensor_location))
+else:
+    sensor_id = found_item["_id"]
+
+print (sensor_id)
+
+currentDate = datetime.datetime.utcnow()
+oneHourAgo = currentDate - datetime.timedelta(seconds=3600)
+
+print (oneHourAgo)
+print (oneHourAgo.isoformat())
+        
+res = measure.aggregate([
+    {"$match":  { "$and" : [
+                       {"SensorCharacteristics": sensor_id},
+                       {"Date": {"$gte" : oneHourAgo.isoformat()}}
+                           ]
+                 }
+    },
+
+    {"$group" : {
+        "_id": None,
+        "count": {"$sum": 1},
+        "x": {"$push": "$Date"},
+        "y": {"$push": "$Temperature"}
+        }
+     }
+])
+
+print (res)
+
+for r in res:
+    print (r)
+    x = np.array(r["x"])
+    y = np.array(r["y"])
+
+    print (len(x))
+
+    print (len(y))
+
+e = mdates.datestr2num(x)
+
+
+fig, ax = plt.subplots()
+plt.plot_date(e, y, linestyle="solid")
+
+plt.title("Sensor values in the last hour")
+ax.fmt_xdata = mdates.DateFormatter('%m-%d %H:%M:%S')
+fig.autofmt_xdate()
+
+plt.show()
+
