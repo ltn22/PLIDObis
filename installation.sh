3562778a3f1016db4006ac254ab7c58270de9dbb
ltn22 <laurent@touta.in>
2021-04-30 10:42:54 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
 # install mongodb
-sh "wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -"
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.4 main" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.4 main" | tee "/etc/apt/sources.list.d/mongodb-org-4.4.list"
 apt-get update
 apt-get install -y mongodb-org
 # Java
