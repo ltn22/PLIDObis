2ae5cdeb3b2899ef7faf696590611ab5c2996570
Laurent Toutain <laurent@touta.in>
2021-06-04 22:00:59 +0200

    bam

---
+++ plido-tp6/bam.py
@@ -0,0 +1,21 @@
+mport numpy as np
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots(4, 2)
+
+X = [i for i in range(0,1000)]
+
+Y = [i for i in range(0, 3)]
+
+Y[0] = [5 for i in range(0, 1000)] # constant
+Y[1] = [i/100.0 for i in range(0,1000)] # linear
+Y[2] = [i%10 for i in range(0, 1000)]
+
+for i in range(0, 3):
+    name = 'courbe_{}.csv'.format(i+1)
+    dat_csv = np.loadtxt(name, delimiter = ',')
+
+    ax[i, 0].plot(X, Y[i],'+')
+    ax[i, 1].boxplot (Y[i])
+plt.show()
