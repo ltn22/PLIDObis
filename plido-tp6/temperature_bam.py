e60b347da2f6ad5a07cc8c8a936143e41318b98a
Laurent Toutain <laurent@touta.in>
2021-06-04 21:56:56 +0200

    bam

--- plido-tp6/temperature_bam.py
+++ plido-tp6/temperature_bam.py
@@ -58,10 +58,10 @@ e = mdates.datestr2num(x)
 
 
 fig, ax = plt.subplots(1, 2)
-ax[0,0].plot_date(e, y, linestyle="solid")
+ax[0].plot_date(e, y, linestyle="solid")
 
 plt.title("Sensor values in the last hour")
-ax[0,0].fmt_xdata = mdates.DateFormatter('%m-%d %H:%M:%S')
+ax[0].fmt_xdata = mdates.DateFormatter('%m-%d %H:%M:%S')
 fig.autofmt_xdate()
 
 plt.show()
