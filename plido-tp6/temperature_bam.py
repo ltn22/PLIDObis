b8728f3c4b177599b3a704ecd835bed1953bd0c3
Laurent Toutain <laurent@touta.in>
2021-06-06 16:23:42 +0200

    bam

--- plido-tp6/temperature_bam.py
+++ plido-tp6/temperature_bam.py
@@ -85,7 +85,7 @@ for idx in range(1, 2):
     pos = 0
     for a in b_2:
         print (nb_curves, block, pos)
-        curve = fig.add_subplot(grid[idx,pos:block])
+        curve = fig.add_subplot(grid[idx,pos:pos+block])
         curve.plot(np.arange(0, len(a)), a)
         pos += block
 
