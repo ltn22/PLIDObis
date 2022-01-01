3d7ef1a87dcb953caa37c03da36f4ed21ab26c73
Laurent Toutain <laurent@touta.in>
2022-01-01 17:07:41 +0100

    simplify keys

--- pycom/lorawan_send_and_receive.py
+++ pycom/lorawan_send_and_receive.py
@@ -10,14 +10,9 @@ mac = lora.mac()
 print ('devEUI: ',  binascii.hexlify(mac))
 
 # create an OTAA authentication parameters
-app_eui = binascii.unhexlify(
-     '70B3D57ED0033AE3'.replace(' ',''))
-#     '0000000000000000'.replace(' ',''))
+app_eui = binascii.unhexlify('0000000000000000'.replace(' ',''))
 
-app_key = binascii.unhexlify(
-     #'B0923EE49E056F29C1482EE5846FADF4'.replace(' ',''))  # TTN
-     #'11223344556677881122334455667788'.replace(' ',''))   # Acklio
-     '11 00 22 00 33 00 44 00 55 00 66 00 77 00 88 00'.replace(' ',''))   # chirpstack
+app_key = binascii.unhexlify('4EAE56D0689F6F8B02C2AFA7E08DADBA'.replace(' ',''))
 
 pycom.heartbeat(False)
 pycom.rgbled(0x101010) # white
