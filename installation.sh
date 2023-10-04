a15b7b05e1265d15ce2137ac510584ab7096687e
Laurent Toutain <laurent@touta.in>
2023-10-04 20:44:52 +0200

    test buster

--- installation.sh
+++ installation.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 #
+
+rm -f /etc/apt/source.list
+echo "deb [trusted=yes] https://deb.debian.org/debian buster main contrib non-fr
+ee" > /etc/apt/sources.list
+echo "deb [trusted=yes] https://deb.debian.org/debian buster-backports main cont
+rib non-free" >> /etc/apt/sources.list
+echo "deb [trusted=yes] file:///usr/share/core-repository ./" >> /etc/apt/source
+s.list
+sudo apt update --fix-missing --allow-unauthenticated
+
 # install mongodb
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.4 main" | tee "/etc/apt/sources.list.d/mongodb-org-4.4.list"
